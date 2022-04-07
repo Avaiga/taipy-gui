3a4c629808af9a6c0073f0735ed7be6e911bca16
dinhlongnguyen <dinhlongviolin1@gmail.com>
2022-04-07 18:38:17 +0700

    Update setup.py
    Co-authored-by: Vuillemot Florian <florian.vuillemot@posteo.net>

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description="AI Platform for Business Applications.",
     long_description=readme,
