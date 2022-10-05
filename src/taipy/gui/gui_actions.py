bc5026b3019a038c4dc660d1439f847a5057bf70
Fabien Lelaquais <fabien@lelaquais.fr>
2022-10-05 15:49:15 +0200

    Re-comments
    (cherry picked from commit 882b1206189734e862b14c75a9616a58101ad27e)

--- src/taipy/gui/gui_actions.py
+++ src/taipy/gui/gui_actions.py
@@ -228,12 +228,12 @@ def invoke_long_callback(
             - The second parameter of this function is set to a bool or an int, depending on the
               conditions under which it is called:
 
-               - If the status function is set to a bool value in its second parameter, then:
+               - If this parameter is set to a bool value, then:
 
                    - If True, this indicates that *user_function* has finished properly.
                    - If False, this indicates that *user_function* failed.
 
-               - If the status function is set to an int value in its second parameter, then this value indicates
+               - If this parameter is set to an int value, then this value indicates
                  how many periods (as lengthy as indicated in *period*) have elapsed since *user_function* was
                  started.
         user_status_function_args (Optional[List|Tuple]): The remaining arguments of the user status function.
