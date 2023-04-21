8685ab8525e260111339130debb5bfc85ff644b1
trgiangdo <dtr.giang.1299@gmail.com>
2023-04-21 15:49:49 +0700

    feat: add GUI CLI args to taipy subcommand

--- src/taipy/gui/_gui_cli.py
+++ src/taipy/gui/_gui_cli.py
@@ -9,15 +9,26 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-from taipy.config.common._argparser import _Argparser
+from argparse import Namespace, ArgumentError
+from taipy.config._cli._argparser import _Argparser
 
 
 class _GuiCLI:
     """Command-line interface of GUI."""
+    _DEFAULT_ARGS = {
+        "port": "",
+        "host": "",
+        "ngrok_token": "",
+        "webapp_path": "",
+        "debug": False,
+        "no_debug": False,
+        "use_reloader": False,
+        "no_reloader": False,
+    }
 
     @classmethod
-    def _create_parser(cls):
-        gui_parser = _Argparser._add_groupparser("GUI", "Optional arguments for GUI service")
+    def create_parser(cls):
+        gui_parser = _Argparser._add_subparser("taipy", help="Run application with Taipy arguments.")
 
         gui_parser.add_argument("-P", "--port", nargs="?", default="", const="", help="Specify server port")
         gui_parser.add_argument("-H", "--host", nargs="?", default="", const="", help="Specify server host")
@@ -40,5 +51,21 @@ class _GuiCLI:
         reloader_group.add_argument("--no-reloader", help="No reload on code changes", action="store_true")
 
     @classmethod
-    def _parse_arguments(cls):
-        return _Argparser._parse()
+    def parse_arguments(cls):
+        try:
+            args = _Argparser._parse()
+        except ArgumentError:
+            return cls.default()
+
+        if getattr(args, "which", None) != "taipy":
+            return cls.default()
+
+        return args
+
+    @classmethod
+    def default(cls):
+        default_args = Namespace()
+        for attr, value in cls._DEFAULT_ARGS.items():
+            setattr(default_args, attr, value)
+
+        return default_args
