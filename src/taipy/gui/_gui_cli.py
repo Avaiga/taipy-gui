55a0b330e85f1460ed2fa0699bbb42cf24abf090
trgiangdo <dtr.giang.1299@gmail.com>
2023-05-17 13:12:02 +0700

    fix: update _CLI import

--- src/taipy/gui/_gui_cli.py
+++ src/taipy/gui/_gui_cli.py
@@ -9,7 +9,7 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-from taipy.cli import _CLI
+from taipy._cli._base_cli import _CLI
 
 
 class _GuiCLI:
