0093caad4b02af65604f4d4a79da28be5f9d6e0f
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2023-09-26 19:01:42 +0200

    isort

--- src/taipy/gui/_warnings.py
+++ src/taipy/gui/_warnings.py
@@ -9,9 +9,9 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+import sys
 import traceback
 import typing as t
-import sys
 import warnings
 
 
